65298f23c99e152c0b6a8f9841545ded1e50c598
seropp <ts.seropps@gmail.com>
2022-03-31 17:24:55 +0300

    commit from new_branch(second branch). Add repository into data layer

--- app/src/main/java/com/example/andersentask1/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/andersentask1/data/repository/UserRepositoryImpl.kt
@@ -1,5 +1,38 @@
 package com.example.andersentask1.data.repository
 
-class UserRepositoryImpl {
+import com.example.andersentask1.data.storage.UserStorage
+import com.example.andersentask1.data.storage.models.UserInfoData
+import com.example.andersentask1.domain.models.UserInfo
+import com.example.andersentask1.domain.repository.UserRepository
 
+class UserRepositoryImpl(private val userStorage: UserStorage) : UserRepository {
+
+    override fun saveUserInfo(userInfo: UserInfo) {
+        userStorage.save(mapToStorage(userInfo = userInfo))
+    }
+
+    override fun getUserInfo(): UserInfo {
+        val user = mapToUserInfo(userStorage.get())
+        return UserInfo(
+            name = user.name,
+            lastName = user.lastName,
+            age = user.age
+        )
+    }
+
+    private fun mapToStorage(userInfo: UserInfo): UserInfoData {
+        return UserInfoData(
+            name = userInfo.name,
+            lastName = userInfo.lastName,
+            age = userInfo.age
+        )
+    }
+
+    private fun mapToUserInfo(userInfoData: UserInfoData): UserInfo {
+        return UserInfo(
+            name = userInfoData.name,
+            lastName = userInfoData.lastName,
+            age = userInfoData.age
+        )
+    }
 }
